25f28a746ace35a933956dfb219488216e16ab30
GargAnshu9468 <a.kgarg9050@gmail.com>
2025-08-11 13:29:33 +0530

    .

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 @echo off
 
-REM Check if virtual environment folder exists
+REM Create virtual environment if not already
 if not exist "ims_env\Scripts\activate.bat" (
     echo Creating virtual environment...
     python -m venv ims_env
@@ -12,14 +12,18 @@ call ims_env\Scripts\activate.bat
 REM Upgrade pip to latest
 python -m pip install --upgrade pip >nul 2>&1
 
-REM Install packages
+REM Pull latest changes from GitHub
+echo Checking for an update...
+git pull
+
+REM Install project requirements
 python -m pip install -r requirements.txt >nul 2>&1
 
-REM Run migrations silently
+REM Make & run migrations silently
 python manage.py makemigrations >nul 2>&1
 python manage.py migrate >nul 2>&1
 
-REM Start server in a new window so the batch file can continue
+REM Start server in a new window
 start "" python manage.py runserver 127.0.0.1:8000
 
 REM Wait a few seconds to let the server start
